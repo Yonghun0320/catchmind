6f795807e75fa5e01032549c4ee26ed53b305fca
Yonghun0320 <syhvs0208@naver.com>
2017-05-19 17:46:35 +0900

    Update QuizTable.java
    aassssssssssssssssssss

--- src/test/QuizTable.java
+++ src/test/QuizTable.java
@@ -3,4 +3,5 @@ package test;
 public class QuizTable {
 	public static String[] quiz = new String[]{"��ī��","������","���̸�","������","����Ǯ","�ߵ���","������","�ǰ���"};
   /*깃허브 테스트*/
+	/*hub test*/
 }
